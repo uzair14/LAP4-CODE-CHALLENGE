5fd4755ed2af5961dde37c3cf4171e52b0eaab5e
Rameez <rameez0khawaja@gmail.com>
2022-08-24 11:21:33 +0100

    Route handling

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, jsonify, redirect, render_template, request
+from flask import Flask, redirect, render_template, request
 import requests
 from flask_cors import CORS
 import random
 import string
-
-from werkzeug import exceptions
 
 app = Flask(__name__)
 CORS(app)
@@ -13,32 +11,45 @@ CORS(app)
 def home():
     return render_template('home.html')
 
-@app.route('/url/<url>', methods=['GET', 'POST'] )
+@app.route('/<url>', methods=['GET', 'POST'] )
 def url(url):
     req = requests.get('https://urlshortnerfutureproof.herokuapp.com/urls')
     data = req.json()
-    # print(data)
     for i in data:
         if url == i['shorturl']:
-            print(i['longurl'])
             return redirect(i['longurl'])
-        # print(i['shorturl'])
     return redirect('http://localhost:5000/')
 
 @app.route('/result', methods=['GET', 'POST'] )
 def result():
     if request.method == 'POST':
+
         longurl = request.form['longurl']
-        length = 10
-        letters = string.ascii_letters
-        shorturl = ''.join(random.choice(letters) for i in range(length))
-        data = {'shorturl': shorturl, 'longurl': longurl}
-        # print(data)
-        req = requests.post('https://urlshortnerfutureproof.herokuapp.com/urls', json = data)
-        print(req)
+
+        req = requests.get('https://urlshortnerfutureproof.herokuapp.com/urls')
+        data = req.json()
+
+        foundurl = False
+        for i in data:
+            if longurl == i['longurl']:
+                shorturl = i['shorturl']
+                foundurl = True
+                
+        if foundurl == False:
+            length = 10
+            letters = string.ascii_letters
+            shorturl = ''.join(random.choice(letters) for i in range(length))
+            dataSent = {'shorturl': shorturl, 'longurl': longurl}
+        
+            post = requests.post('https://urlshortnerfutureproof.herokuapp.com/urls', json = dataSent)
+        
         return render_template('result.html', shorturl = shorturl, longurl = longurl)
     else:
         return redirect('http://localhost:5000/')
+
+@app.errorhandler(404)
+def handle_404(e):
+    return redirect('http://localhost:5000/')
 
 
 # # if __name__ == '__main__':
